b61e4b7ef95c58ff3c3db22d75bbb7c7b113f509
Evan Kluska <evankluska@MacBook-Pro-de-Evan.local>
2025-10-08 14:54:45 +0200

    fix: remove duplicate PORT declaration

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const path = require("path");
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
-const PORT = 3000;
 
 app.use(express.static(path.join(__dirname)));
 
